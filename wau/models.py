96307982abda97ad04c55b9648022cdb73112928
NarimanOusman <narimanrobo8@gmail.com>
2026-04-18 06:57:50 -0700

    Refactor media handling: remove Cloudinary integration, update project templates, and streamline admin interface

--- wau/models.py
+++ wau/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.core.exceptions import ValidationError
 
 class Project(models.Model):
     STATUS_CHOICES = [
@@ -17,33 +16,9 @@ class Project(models.Model):
         validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
     description = models.TextField()
-    image = models.ImageField(upload_to='projects/', blank=True, null=True)
-    video = models.FileField(upload_to='projects/videos/', blank=True, null=True)
-    cover_media_url = models.URLField(blank=True, null=True)
-    cover_media_type = models.CharField(max_length=10, blank=True, default='image')
+    image = models.ImageField(upload_to='projects/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def clean(self):
-        super().clean()
-        if not any([self.image, self.video, self.cover_media_url]):
-            raise ValidationError('Add a cover image, cover video, or cover media URL for each project.')
-
-    @property
-    def cover_is_video(self):
-        if self.cover_media_url:
-            return self.cover_media_type == 'video'
-        return bool(self.video)
-
-    @property
-    def cover_media_source(self):
-        if self.cover_media_url:
-            return self.cover_media_url
-        if self.video:
-            return self.video.url
-        if self.image:
-            return self.image.url
-        return ''
 
     def __str__(self):
         return self.title
@@ -51,10 +26,7 @@ class Project(models.Model):
 
 class ProjectImage(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='projects/gallery/', blank=True, null=True)
-    video = models.FileField(upload_to='projects/gallery/videos/', blank=True, null=True)
-    media_url = models.URLField(blank=True, null=True)
-    media_type = models.CharField(max_length=10, blank=True, default='image')
+    image = models.ImageField(upload_to='projects/gallery/')
     caption = models.CharField(max_length=200, blank=True)
     order = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -62,27 +34,5 @@ class ProjectImage(models.Model):
     class Meta:
         ordering = ['order', '-created_at']
 
-    def clean(self):
-        super().clean()
-        if not any([self.image, self.video, self.media_url]):
-            raise ValidationError('Add an image, video, or media URL for each gallery item.')
-
-    @property
-    def is_video(self):
-        if self.media_url:
-            return self.media_type == 'video'
-        return bool(self.video)
-
-    @property
-    def media_source(self):
-        if self.media_url:
-            return self.media_url
-        if self.video:
-            return self.video.url
-        if self.image:
-            return self.image.url
-        return ''
-
     def __str__(self):
-        media_type = 'video' if self.is_video else 'image'
-        return f'{self.project.title} {media_type} {self.order}'
+        return f'{self.project.title} image {self.order}'
--- wau/views.py
+++ wau/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Avg
 
 def home(request):
@@ -60,4 +60,4 @@ def contact(request):
 
 
 def admin_page(request):
-    return redirect('/admin/')
+    return render(request, 'admin.html')
